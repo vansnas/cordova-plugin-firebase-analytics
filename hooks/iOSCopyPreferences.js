8835a8f4050305ecb7f4d540dc4304850e8f7398
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2022-04-18 18:23:31 +0100

    refactor: clean code

--- hooks/iOSCopyPreferences.js
+++ hooks/iOSCopyPreferences.js
@@ -1,9 +1,7 @@
-const et = require('elementtree');
 const path = require('path');
 const fs = require('fs');
 const plist = require('plist');
 const { ConfigParser } = require('cordova-common');
-const { Console } = require('console');
 
 module.exports = function (context) {
     var projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
@@ -12,42 +10,14 @@ module.exports = function (context) {
     var userTrackingDescription = configParser.getGlobalPreference("USER_TRACKING_DESCRIPTION_IOS");
 
     if(userTrackingDescription != ""){
-        /*
-        var appNamePath = path.join(projectRoot, 'config.xml');
-        var appNameParser = new ConfigParser(appNamePath);
-        var appName = appNameParser.name();
-
-        //Change info.plist
-        var infoPlistPath = path.join(projectRoot, 'platforms/ios/' + appName + '/'+ appName +'-info.plist');
-        var infoPlistFile = fs.readFileSync(infoPlistPath).toString();
-        var etreeInfoPlist = et.parse(infoPlistFile);
-        //var infoPlistTags = etreeInfoPlist.findall('./dict/key[text()="NSUserTrackingUsageDescription"]/following-sibling::string');
-
-        var infoPlistTags = etreeInfoPlist.findall('./dict/string');
-
-        for (var i = 0; i < infoPlistTags.length; i++) {
-            console.log("entrou no for");
-            if (infoPlistTags[i].text.includes("os_user_tracking_descrption_placeholder")) {
-                console.log("entrou no if");
-                infoPlistTags[i].text = infoPlistTags[i].text.replace('os_user_tracking_descrption_placeholder', userTrackingDescription);
-            }
-        }
-
-        var resultXmlInfoPlist = etreeInfoPlist.write();
-        fs.writeFileSync(infoPlistPath, resultXmlInfoPlist);
-        */
-
         var appNamePath = path.join(projectRoot, 'config.xml');
         var appNameParser = new ConfigParser(appNamePath);
         var appName = appNameParser.name();
 
         var infoPlistPath = path.join(projectRoot, 'platforms/ios/' + appName + '/'+ appName +'-info.plist');
-
 
         var obj = plist.parse(fs.readFileSync(infoPlistPath, 'utf8'));
-        obj['NSUserTrackingUsageDescription'] = 'hello'
-        console.log(JSON.stringify(obj));
+        obj['NSUserTrackingUsageDescription'] = userTrackingDescription;
         fs.writeFileSync(infoPlistPath, plist.build(obj));
-        
     }
 };
