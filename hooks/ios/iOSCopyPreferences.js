e17153712219f3b293a1e27ff39d9910d02c31ff
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2022-05-06 17:31:30 +0100

    test: testing deleting NSUserTrackingUsageDescription

--- hooks/ios/iOSCopyPreferences.js
+++ hooks/ios/iOSCopyPreferences.js
@@ -2,22 +2,28 @@ const path = require('path');
 const fs = require('fs');
 const plist = require('plist');
 const { ConfigParser } = require('cordova-common');
+const { config } = require('process');
 
 module.exports = function (context) {
     var projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
     var configXML = path.join(projectRoot, 'config.xml');
     var configParser = new ConfigParser(configXML);
-    var userTrackingDescription = configParser.getGlobalPreference("USER_TRACKING_DESCRIPTION_IOS");
+    var enableAppTracking = configParser.getPlatformPreference("EnableAppTrackingTransparencyPrompt", "ios");
 
-    if(userTrackingDescription != ""){
-        var appNamePath = path.join(projectRoot, 'config.xml');
-        var appNameParser = new ConfigParser(appNamePath);
-        var appName = appNameParser.name();
+    var appNamePath = path.join(projectRoot, 'config.xml');
+    var appNameParser = new ConfigParser(appNamePath);
+    var appName = appNameParser.name();
+    var infoPlistPath = path.join(projectRoot, 'platforms/ios/' + appName + '/'+ appName +'-info.plist');
+    var obj = plist.parse(fs.readFileSync(infoPlistPath, 'utf8'));
 
-        var infoPlistPath = path.join(projectRoot, 'platforms/ios/' + appName + '/'+ appName +'-info.plist');
-
-        var obj = plist.parse(fs.readFileSync(infoPlistPath, 'utf8'));
-        obj['NSUserTrackingUsageDescription'] = userTrackingDescription;
-        fs.writeFileSync(infoPlistPath, plist.build(obj));
+    if(enableAppTracking == true){
+        if(userTrackingDescription != ""){
+            var userTrackingDescription = configParser.getPlatformPreference("USER_TRACKING_DESCRIPTION_IOS", "ios");
+            obj['NSUserTrackingUsageDescription'] = userTrackingDescription;
+        }
     }
+    else{
+        obj['NSUserTrackingUsageDescription'] = null;
+    }
+    fs.writeFileSync(infoPlistPath, plist.build(obj));
 };
