ff177de349c16bcec0304cbdd9a3bcd32325c896
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2022-05-09 13:34:49 +0100

    revert some of the changes in previous commit

--- www/FirebaseAnalytics.js
+++ www/FirebaseAnalytics.js
@@ -48,23 +48,25 @@ module.exports = {
 
             exec(resolve, reject, PLUGIN_NAME, "setDefaultEventParameters", [defaults || {}]);
         });
+    },
+    requestTrackingAuthorization: function(success, error, showInformation, title, message, buttonTitle) {
+        return new Promise(function(resolve, reject) {
+
+            if(showInformation) {
+                if (typeof title !== "string") {
+                    return reject(new TypeError("Title property name must be a string"));
+                }
+    
+                if (typeof message !== "string") {
+                    return reject(new TypeError("Message property value must be a string"));
+                }
+    
+                if (typeof buttonTitle !== "string") {
+                    return reject(new TypeError("Button title property value must be a string"));
+                }
+            }
+
+            exec(resolve, reject, PLUGIN_NAME, "requestTrackingAuthorization", [showInformation, title, message, buttonTitle]);
+        });
     }
-};
-
-exports.requestTrackingAuthorization = function (success, error, showInformation, title, message, buttonTitle) {
-
-    if(showInformation) {
-        if (typeof title !== "string") {
-            return error(new TypeError("Title property name must be a string"));
-        }
-
-        if (typeof message !== "string") {
-            return error(new TypeError("Message property value must be a string"));
-        }
-
-        if (typeof buttonTitle !== "string") {
-            return error(new TypeError("Button title property value must be a string"));
-        }
-    }
-    exec(success, error, PLUGIN_NAME, 'requestTrackingAuthorization', [showInformation, title, message, buttonTitle]);
 };
