704c8dc62049811cf7789f6c10123ee76c2a7fb0
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2022-05-09 12:05:38 +0100

    test: include log for testing

--- hooks/ios/iOSCopyPreferences.js
+++ hooks/ios/iOSCopyPreferences.js
@@ -10,13 +10,15 @@ module.exports = function (context) {
     var configParser = new ConfigParser(configXML);
     var enableAppTracking = configParser.getPlatformPreference("EnableAppTrackingTransparencyPrompt", "ios");
 
+    console.log("ENABLE_TRACKING" + enableAppTracking);
+
     var appNamePath = path.join(projectRoot, 'config.xml');
     var appNameParser = new ConfigParser(appNamePath);
     var appName = appNameParser.name();
     var infoPlistPath = path.join(projectRoot, 'platforms/ios/' + appName + '/'+ appName +'-info.plist');
     var obj = plist.parse(fs.readFileSync(infoPlistPath, 'utf8'));
 
-    if(enableAppTracking == true){
+    if(enableAppTracking == "true"){
         if(userTrackingDescription != ""){
             var userTrackingDescription = configParser.getPlatformPreference("USER_TRACKING_DESCRIPTION_IOS", "ios");
             obj['NSUserTrackingUsageDescription'] = userTrackingDescription;
